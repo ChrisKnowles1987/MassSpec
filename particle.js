e3e192a045e1efce1200d433a326af6b8eb8ba66
ChrisK <chrisknowles1987@me.com>
2023-10-24 15:45:39 +0100

    got move and draw methods working

--- particle.js
+++ particle.js
@@ -1,41 +1,52 @@
 class Particle {
-    constructor(x,y) {
+    constructor(x, y) {
         this.x = x
         this.y = y
         this.mass = 100
-        this.charge=1
+        this.charge = 1
         this.colour = this.determineColour(this.charge)
-        
+
     }
-    determineColour(charge){
-        let coloursList = ['red','yellow','green']
+    determineColour(charge) {
+        let coloursList = ['red', 'yellow', 'green']
         this.colour = coloursList[this.charge]
         return this.colour
-        
-    }
-    draw(){
-       ctx.beginPath();
-       ctx.arc(75,75, this.mass / 50, 0, Math.PI * 2);
-       ctx.fillStyle=(this.colour);
-       ctx.fill();
-       ctx.closePath();
-    }
-    move(){
-        this.x=this.x+1
-        this.y=this.y
-    }
-}
-particle = new Particle(100,100)
 
-window.requestAnimationFrame(draw);
-function draw(){
-    
- 
- 
- particle.move(this.x,this.y)
- particle.draw();
- console.log(particle);
- console.log(particle.colour)
- window.requestAnimationFrame(draw);
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.mass / 50, 0, Math.PI * 2);
+        ctx.fillStyle = (this.colour);
+        ctx.fill();
+        ctx.closePath();
+    }
+    move() {
+        this.x = this.x + 1
+        this.y = this.y
+    }
 }
- 
+
+
+for (i=0; i<100; i++) {
+    particles.push(new Particle(100,100))
+}
+
+
+particles = [
+    new Particle(100, 100),
+    new Particle(100, 100),
+    new Particle(100, 100)
+]
+window.requestAnimationFrame(draw);
+function draw() {
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb(40,40,40)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    particle.move();
+    particle.draw();
+
+
+    window.requestAnimationFrame(draw);
+}
