d4d16cfbf9e4238e8c77d487c3e1297b84eb9952
ChrisK <chrisknowles1987@me.com>
2023-10-25 10:11:16 +0100

    Movement

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth
+
 canvas.height = window.innerHeight
 //canvas.style.background = 'black'
 ctx = canvas.getContext('2d');
@@ -8,5 +9,31 @@ ctx = canvas.getContext('2d');
 ctx.fillStyle = 'rgb(40,40,40)';
 ctx.fillRect(0,0,canvas.width, canvas.height);
 
+let particles = [];
+for (i=0; i<1000; i++) {
+    particles.push(new Particle(100,100, 
+        Math.random() *200,
+        // <0.5? 100: 200 , 
+        Math.ceil(Math.random() * 4
+        )))
+}
+
+window.requestAnimationFrame(draw);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgb(40,40,40)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(particle => {
+        particle.move();
+        particle.draw();
+    });
+
+
+    
+    
+
+
+    window.requestAnimationFrame(draw);
+}
 
 
