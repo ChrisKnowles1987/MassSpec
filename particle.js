f68a28eefd7f9b90fa29ced1678291e6ae6248d7
ChrisK <chrisknowles1987@me.com>
2023-10-25 10:11:36 +0100

    Movement

--- particle.js
+++ particle.js
@@ -1,52 +1,52 @@
 class Particle {
-    constructor(x, y) {
+    constructor(x, y,mass, charge) {
         this.x = x
         this.y = y
-        this.mass = 100
-        this.charge = 1
+        this.mass = mass
+        this.charge = charge
         this.colour = this.determineColour(this.charge)
+        this.Xvelocity = 1
+        this.Yvelocity = 0.0
+        
 
     }
     determineColour(charge) {
-        let coloursList = ['red', 'yellow', 'green']
-        this.colour = coloursList[this.charge]
+        let coloursList = ['red', 'yellow', 'white', 'blue']
+        this.colour = coloursList[this.charge-1]
         return this.colour
 
     }
     draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.mass / 50, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, 1+ (this.mass / 50), 0, Math.PI * 2);
         ctx.fillStyle = (this.colour);
         ctx.fill();
         ctx.closePath();
     }
     move() {
-        this.x = this.x + 1
-        this.y = this.y
+        
+        this.x = this.x + this.Xvelocity
+        this.y = this.y +this.Yvelocity
+        
+        
+
+        this.Xvelocity+=0.1 * this.mass /100
+        this.Yvelocity+= this.Yvelocity * this.charge /100
+        
+        
+        
+
+        if(this.x > canvas.width ){
+            this.x =100
+            this.y = 100
+            this.Xvelocity = 0.01
+            this.Yvelocity = 0.0
+        }
+
+        
+
     }
 }
 
 
-for (i=0; i<100; i++) {
-    particles.push(new Particle(100,100))
-}
 
-
-particles = [
-    new Particle(100, 100),
-    new Particle(100, 100),
-    new Particle(100, 100)
-]
-window.requestAnimationFrame(draw);
-function draw() {
-
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'rgb(40,40,40)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    particle.move();
-    particle.draw();
-
-
-    window.requestAnimationFrame(draw);
-}
